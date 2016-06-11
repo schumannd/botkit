c96c308fe85120658e85a1161e1204a95e4f1c6a
David Schumann <david.schumann@student.hpi.de>
2016-06-12 00:12:28 +0200

    DIsabld tunnel

--- facebook_bot.js
+++ facebook_bot.js
@@ -107,20 +107,20 @@ var bot = controller.spawn({
 controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
-        if(ops.lt) {
-            var tunnel = localtunnel(process.env.PORT || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
-                if (err) {
-                    console.log(err);
-                    process.exit();
-                }
-                console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
-            });
+        // if(ops.lt) {
+        //     var tunnel = localtunnel(process.env.PORT || 3000, {subdomain: ops.ltsubdomain}, function(err, tunnel) {
+        //         if (err) {
+        //             console.log(err);
+        //             process.exit();
+        //         }
+        //         console.log("Your bot is available on the web at the following URL: " + tunnel.url + '/facebook/receive');
+        //     });
 
-            tunnel.on('close', function() {
-                console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
-                process.exit();
-            });
-        }
+        //     tunnel.on('close', function() {
+        //         console.log("Your bot is no longer available on the web at the localtunnnel.me URL.");
+        //         process.exit();
+        //     });
+        // }
     });
 });
 
